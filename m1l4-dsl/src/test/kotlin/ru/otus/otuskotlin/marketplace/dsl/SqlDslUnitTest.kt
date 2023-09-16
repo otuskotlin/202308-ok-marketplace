af8cebf341bc238d4e85f443648e84c2b764b494
evgeniyN <enji81@gmail.com>
2023-09-16 08:15:12 +0300

    m1l4 - DSL - решение sql

--- m1l4-dsl/src/test/kotlin/ru/otus/otuskotlin/marketplace/dsl/SqlDslUnitTest.kt
+++ m1l4-dsl/src/test/kotlin/ru/otus/otuskotlin/marketplace/dsl/SqlDslUnitTest.kt
@@ -1,57 +1,58 @@
 package ru.otus.otuskotlin.marketplace.dsl
 
 import org.junit.Test
+import ru.otus.otuskotlin.marketplace.sql.*
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 // Реализуйте dsl для составления sql запроса, чтобы все тесты стали зелеными.
 class SqlDslUnitTest {
-//    private fun checkSQL(expected: String, sql: SqlSelectBuilder) {
-//        assertEquals(expected, sql.build())
-//    }
+    private fun checkSQL(expected: String, sql: SqlSelectBuilder) {
+        assertEquals(expected, sql.build())
+    }
 
     @Test
     fun `simple select all from table`() {
-//        val expected = "select * from table"
-//
-//        val real = query {
-//            from("table")
-//        }
-//
-//        checkSQL(expected, real)
+        val expected = "select * from table"
+
+        val real = query {
+            from("table")
+        }
+
+        checkSQL(expected, real)
     }
 
     @Test
     fun `check that select can't be used without table`() {
-//        assertFailsWith<Exception> {
-//            query {
-//                select("col_a")
-//            }.build()
-//        }
+        assertFailsWith<Exception> {
+            query {
+                select("col_a")
+            }.build()
+        }
     }
 
     @Test
     fun `select certain columns from table`() {
-//        val expected = "select col_a, col_b from table"
-//
-//        val real = query {
-//            select("col_a", "col_b")
-//            from("table")
-//        }
-//
-//        checkSQL(expected, real)
+        val expected = "select col_a, col_b from table"
+
+        val real = query {
+            select("col_a", "col_b")
+            from("table")
+        }
+
+        checkSQL(expected, real)
     }
 
     @Test
     fun `select certain columns from table 1`() {
-//        val expected = "select col_a, col_b from table"
-//
-//        val real = query {
-//            select("col_a", "col_b")
-//            from("table")
-//        }
-//
-//        checkSQL(expected, real)
+        val expected = "select col_a, col_b from table"
+
+        val real = query {
+            select("col_a", "col_b")
+            from("table")
+        }
+
+        checkSQL(expected, real)
     }
 
     /**
@@ -62,14 +63,14 @@ class SqlDslUnitTest {
      */
     @Test
     fun `select with complex where condition with one condition`() {
-//        val expected = "select * from table where col_a = 'id'"
-//
-//        val real = query {
-//            from("table")
-//            where( "col_a" eq "id")
-//        }
-//
-//        checkSQL(expected, real)
+        val expected = "select * from table where col_a = 'id'"
+
+        val real = query {
+            from("table")
+            where( "col_a" eq "id")
+        }
+
+        checkSQL(expected, real)
     }
 
     /**
@@ -80,30 +81,30 @@ class SqlDslUnitTest {
      */
     @Test
     fun `select with complex where condition with two conditions`() {
-//        val expected = "select * from table where col_a != 0"
-//
-//        val real = query {
-//            from("table")
-//            where("col_a" nonEq 0)
-//        }
-//
-//        checkSQL(expected, real)
+        val expected = "select * from table where col_a != 0"
+
+        val real = query {
+            from("table")
+            where("col_a" nonEq 0)
+        }
+
+        checkSQL(expected, real)
     }
 
     @Test
     fun `when 'or' conditions are specified then they are respected`() {
-//        val expected = "select * from table where (col_a = 4 or col_b !is null or col_c !is null)"
-//
-//        val real = query {
-//            from("table")
-//            where(
-//                or(
-//                    "col_a" eq 4,
-//                    "col_b" nonEq null,
-//                    "col_c" nonEq null)
-//            )
-//        }
-//
-//        checkSQL(expected, real)
+        val expected = "select * from table where (col_a = 4 or col_b !is null or col_c !is null)"
+
+        val real = query {
+            from("table")
+            where(
+                or(
+                    "col_a" eq 4,
+                    "col_b" nonEq null,
+                    "col_c" nonEq null)
+            )
+        }
+
+        checkSQL(expected, real)
     }
 }
